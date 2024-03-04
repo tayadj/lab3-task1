6aeef3e5f6d8fa4d5dea10b0176d99869353cc4d
tayadj <144615941+tayadj@users.noreply.github.com>
2024-03-04 09:02:26 +0000

    Added some code

--- .ccls-cache/@home@runner@lab3-task1-gr11b-filippov/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr11b-filippov/src@main.c
@@ -1,8 +1,16 @@
 /* hello.c */
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+void main(void) {
+  char input[100];
+  for (int i = 0; i < 100; i++) {
+    input[i] = 0;
+  }
+  
+  scanf("%100s", input);
+  for (int i = 0; i < 100; i++) {
+    if (input[i] != 0) {
+      printf("%c%d", input[i], input[i]);
+    } else { break; }    
+  }
 }
